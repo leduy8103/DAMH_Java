d8a1f5e589bba8aa61b061aa76614e2322c3918a
HoangNhism <hoangtranminh2003@gmail.com>
2024-06-17 22:29:18 +0700

    update admin controller

--- DAJava/src/main/java/com/example/DAJava/repository/SongsRepository.java
+++ DAJava/src/main/java/com/example/DAJava/repository/SongsRepository.java
@@ -3,5 +3,8 @@ package com.example.DAJava.repository;
 import com.example.DAJava.model.Songs;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SongsRepository extends JpaRepository<Songs, Long> {
+    List<Songs> findByTitleContainingIgnoreCase(String title);
 }
